6d9c0ac569983fa1006a9ac280c70d26ea3b5262
ahussain33 <113541906+ahussain33@users.noreply.github.com>
2024-04-10 20:08:36 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,10 +7,16 @@ function init(){
     text = ('Thank you ' + input.value + ', you should receive a text message soon!');
     alert(text);
   }
-  
+}
+
+function initMap(){
   const myLatLng = {lat: 41.257210, lng: -95.965462};
   const map = new google.maps.Map(document.getElementById("map"), {zoom: 15, center: myLatLng,});
-
+ 
+  marker.addListener("click", () =>
+    {infowindow.open({anchor: 
+      marker, map, });
+      });
 }
 window.addEventListener('load', init);
-window.init = init;
+window.initMap = initMap;
