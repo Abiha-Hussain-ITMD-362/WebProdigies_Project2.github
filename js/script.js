627403348c6510e2e78f46a5872a073d56003874
ahussain33 <113541906+ahussain33@users.noreply.github.com>
2024-04-10 19:40:31 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,10 +3,14 @@ function init(){
   var input = document.getElementById("name");
   button.addEventListener("click", myFunction);
  
-
   function myFunction() {
     text = ('Thank you ' + input.value + ', you should receive a text message soon!');
     alert(text);
   }
+  
+  const myLatLng = {lat: 41.257210, lng: -95.965462};
+  const map = new google.maps.Map(document.getElementById("map"), {zoom: 15, center: myLatLng,});
+
 }
 window.addEventListener('load', init);
+window.init = init;
