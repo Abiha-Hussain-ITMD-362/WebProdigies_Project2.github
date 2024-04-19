ec2d65aacb8d8d25f8c9a386928fa2d49efee639
ahussain33 <113541906+ahussain33@users.noreply.github.com>
2024-04-19 11:57:48 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,13 +4,18 @@ function init(){
   button.addEventListener("click", myFunction);
  
   function myFunction() {
-    text = ('Thank you ' + input.value + ', you should receive a text message soon!');
-    alert(text);
+    if (input.value.length == 0) {
+      text = ('Please Enter Your Contact Info!');
+      alert(text);
+    } else if(){
+      text = ('Thank you ' + input.value + ', you should receive a text message soon!');
+      alert(text);
+    }
   }
 }
 function initMap(){
   const myLatLng = {lat: 41.257210, lng: -95.965462};
-  const map = new google.maps.Map(document.getElementById("map"), {zoom: 15, center: myLatLng,});
+  const map = new google.maps.Map(document.getElementById("map"), {zoom: 20, center: myLatLng,});
   
   const marker = new google.maps.Marker({
     position: myLatLng,
